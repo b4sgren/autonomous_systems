7ab83c2c05e7f032d5eca78032c207503ddd3c27
Brendon Forsgren <bforsgren29@gmail.com>
2019-09-23 21:01:37 -0600

    Verified the measurement model is working

--- ekf/main.py
+++ ekf/main.py
@@ -5,36 +5,38 @@ import car_params as params
 import scipy.io as sio
 from ekf import EKF
 
+from IPython.core.debugger import Pdb
+
 def generateVelocities(t):
     v = np.zeros_like(t)
     w = np.zeros_like(t)
 
-    for i in range(t.size): #Note: still need to add noise to these
+    for i in range(t.size):
         v[i] = 1 + 0.5 * np.cos(2 * np.pi * 0.2 * t[i])
         w[i] = -0.2 + 2 * np.cos(2 * np.pi * 0.6 * t[i])
 
     return v, w
 
 def getMeasurements(state):
-    z = np.zeros_like(params.lms)
+    z = np.zeros_like(params.lms, dtype=float)
 
     for i in range(z.shape[1]):
         lm = params.lms[:,i]
-        ds = state[0:2] - lm
+        ds = lm - state[0:2]
 
         r = np.sqrt(np.sum(ds**2))
         theta = np.arctan2(ds[1], ds[0]) - state[2]
 
         z[0,i] = r + np.random.normal(0, params.sigma_r)
         z[1,i] = theta + np.random.normal(0, params.sigma_theta)
-    
+
     return z
 
 if __name__ == "__main__":
-    t = np.arange(0, params.tf + .01, params.dt)
+    t = np.arange(0, params.tf, params.dt)
     vc, wc = generateVelocities(t)
     v = vc + np.sqrt(params.alpha1 * vc**2 + params.alpha2 * wc**2) * np.random.randn(vc.size)
-    w = wc + np.sqrt(params.alpha3 * vc**2 + params.alpha4 * wc**2) * np.random.randn(wc.size) 
+    w = wc + np.sqrt(params.alpha3 * vc**2 + params.alpha4 * wc**2) * np.random.randn(wc.size)
 
     Car = CarAnimation()
     ekf = EKF(params.dt)
@@ -50,10 +52,10 @@ if __name__ == "__main__":
         Car.animateCar(state, mu, dead_reckon)
         plt.pause(0.02)
 
-        state = ekf.propagateState(state, v[i], w[i])
-        dead_reckon = ekf.propagateState(dead_reckon, vc[i], w[i])
-        
         zt = getMeasurements(state)
+
+        state = ekf.propagateState(state, v[i], w[i])
+        dead_reckon = ekf.propagateState(dead_reckon, vc[i], wc[i])
 
     print("Finished")
     plt.waitforbuttonpress()

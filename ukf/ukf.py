1fbf6a35f373cebc3b97c04fa724594b9607b035
Brendon Forsgren <bforsgren29@gmail.com>
2019-09-30 16:49:27 -0600

    Not working yet. Just tried it. Need to start debugging

--- ukf/ukf.py
+++ ukf/ukf.py
@@ -8,10 +8,10 @@ def unwrap(phi):
     # Pdb().set_trace()
     # phi[phi >= np.pi] -= 2 * np.pi # not sure that this will work
     # phi[phi < -np.pi] += 2 * np.pi
-    while phi >= np.pi:
-        phi = phi - 2 * np.pi
-    while phi < -np.pi:
-        phi = phi + 2 * np.pi
+    # while phi >= np.pi:
+    #     phi = phi - 2 * np.pi
+    # while phi < -np.pi:
+    #     phi = phi + 2 * np.pi
     return phi
 
 
@@ -66,15 +66,29 @@ class UKF:
         Chi_a = self.generateSigmaPoints(mu_a, L)
 
         #propagation step
-        Pdb().set_trace()
+        # Pdb().set_trace()
         Chi_x_bar = self.propagateSigmaPts(Chi_a[0:3,:], Chi_a[3:5,:], v, w)
         mu_bar = np.sum(self.wm * Chi_x_bar, axis=1)
-        temp = Chi_x_bar - mu_bar.reshape((3,1))
-        Sigma_bar = np.sum(np.einsum('ij, kj->jik', temp, temp), axis=0)
+        temp_x = Chi_x_bar - mu_bar.reshape((3,1))
+        Sigma_bar = np.sum(self.wc.reshape(2*params.n + 1, 1, 1) * np.einsum('ij, kj->jik', temp_x, temp_x), axis=0) # TODO: Make sure this works correctly
 
-        #Measurement updates
-        for i in range(z.shape[1]):
-            Z_bar = self.generateObservationSigmas(Chi_a[0:3,:], Chi_a[5:, :])
+        #Measurement updates TODO: Make the first one out of the loop maybe
+        # for i in range(z.shape[1]):
+        for i in range(1): #Start with just the first landmark only
+            Z_bar = self.generateObservationSigmas(Chi_x_bar, Chi_a[5:, :], params.lms[:,i])
+            z_hat = np.sum(self.wm * Z_bar, axis=1) # Make sure that this does what I want it to
+            temp_z = Z_bar - z_hat.reshape((2, 1))
+
+            S = np.sum(self.wc.reshape(2 * params.n + 1, 1, 1) * np.einsum('ij, kj->jik', temp_z, temp_z), axis=0)
+            Sigma_xz = np.sum(self.wc.reshape(2 * params.n+1, 1, 1) * np.einsum('ij, kj->jik', temp_x, temp_z), axis=0) # TODO: Make sure this works correctly
+
+            #Calculate the kalman gain
+            K = Sigma_xz @ np.linalg.inv(S)
+            mu_bar = mu_bar + K @ (z[:,i] - z_hat)
+            Sigma_bar = Sigma_bar - K @ S @ K.T
+
+            #redraw sigma points (if not the last lm) and then reset stuff. Chi_x_bar?, temp_x, 
+        
 
     def augmentState(self, mu, Sigma, v, w):
         M = np.diag([params.alpha1 * v**2 + params.alpha2 * w**2, params.alpha3 * v**2 + params.alpha4 * w**2])
@@ -95,6 +109,15 @@ class UKF:
 
         return Chi_a
     
-    def generateObservationSigmas(self, Chi_x, Chi_z):
+    def generateObservationSigmas(self, Chi_x, Chi_z, lm):
         xy = Chi_x[0:2,:]
         thetas = Chi_x[2,:]
+
+        ds = lm.reshape((2,1)) - xy 
+        r = np.sqrt(np.sum(ds**2, axis=0)) 
+
+        psi = np.arctan2(ds[1,:], ds[0,:]) - thetas #not sure it should be thetas or the theta in mu_bar???
+        psi = unwrap(psi)
+
+        Z = np.vstack((r, psi)) + Chi_z
+        return Z

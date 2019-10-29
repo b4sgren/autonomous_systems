3d1215cfba6919a8520a75d9c190e115a32c36f6
Brendon Forsgren <bforsgren29@gmail.com>
2019-10-28 19:41:04 -0600

    It alomost looks like I'm moving the wrong way. Like my movement should be transposed

--- occupancy_grid/occupancy_grid.py
+++ occupancy_grid/occupancy_grid.py
@@ -19,8 +19,8 @@ class OccupancyGrid:
         L = np.zeros_like(self.map)
         for i in range(params.l):
             for j in range(params.w):
-                dx = self.Xi[j,i] - pose[0]
-                dy = self.Yi[j,i] - pose[1]
+                dx = self.Xi[i,j] - pose[0]
+                dy = self.Yi[i,j] - pose[1]
 
                 r = np.sqrt(dx**2 + dy**2)
                 phi = np.arctan2(dy,dx) - pose[2]
@@ -28,11 +28,11 @@ class OccupancyGrid:
                 k = np.argmin(np.abs(phi - (pose[2] - params.thk)))
 
                 if r > np.minimum(params.z_max, z[0,k] + params.alpha/2.0) or np.abs(phi - z[1,k]) > params.beta/2.0:
-                    L[j,i] = self.l0
+                    L[i,j] = self.l0
                 elif z[0,k] < params.z_max and np.abs(r - z[0,k]) < params.alpha/2.0:
-                    L[j,i] = np.log(params.p_occ/(1 - params.p_occ))
+                    L[i,j] = np.log(params.p_occ/(1 - params.p_occ))
                 elif r < z[0,k]:
-                    L[j,i] = np.log(params.p_emp/(1 - params.p_emp))
+                    L[i,j] = np.log(params.p_emp/(1 - params.p_emp))
         L_map = np.log(self.map/(1 - self.map))
         L_map += L - self.l0
         self.map = 1.0 / (1 + np.exp(L_map))

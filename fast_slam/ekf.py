The ekf update runs. Need to check if the Jacobian is the correct jacobian

fast_slam/ekf.py
@@ -41,27 +41,15 @@ class EKF:
 
         return z_hat, H
 
-    def update(self, mu, z, v, w):
-        for i in range(z.shape[1]):
-            lm = params.lms[:,i]
-            ds = lm - self.mu[0:2]
+    def update(self, z, pose):
+        z_hat, H = self.getExpectedMeasurement(z, pose) #Verify that this is the correct H to use
 
-            r = np.sqrt(ds @ ds)
-            phi = np.arctan2(ds[1], ds[0]) - self.mu[2] 
-            phi = unwrap(phi)
-            z_hat = np.array([r, phi])
+        S = H @ self.Sigma @ H.T + self.Q   #Innovation Covariance
+        K = self.Sigma @ H.T @ np.linalg.inv(S)
 
-            # I believe that H changes for this
-            H = np.array([[-(lm[0] - self.mu[0])/r, -(lm[1] - self.mu[1])/r, 0],
-                          [(lm[1] - self.mu[1])/r**2, -(lm[0] - self.mu[0])/r**2, -1]])
+        innov = z - z_hat
+        innov[1] = unwrap(innov[1])
+        self.mu = self.mu + K @ (innov) 
+        self.Sigma = (np.eye(2) - K @ H) @ self.Sigma
 
-            S = H @ self.Sigma @ H.T + self.Q
-            K = self.Sigma @ H.T @ np.linalg.inv(S)
-
-            innov = z[:,i] - z_hat
-            innov[1] = unwrap(innov[1])
-            self.mu = self.mu + K @ (innov) 
-            self.mu[2] = unwrap(self.mu[2])
-            self.Sigma = (np.eye(3) - K @ H) @ self.Sigma
-
-        self.mu[2] = unwrap(self.mu[2])
+        return S, innov

fast_slam/particle_filter.py
@@ -77,7 +77,9 @@ class ParticleFilter:
                     self.lm_filters[i][lm].found = True
                     #Initialize filter
                     self.lm_filters[i][lm].initialize(z[:,j], Chi[:,i])
-                    w[i] = 1.0 / len(w) # Is this right? Do I reinitialize the weight if a new LM is found?
+                    w[i] = 1.0 / len(w) # Is this right? Do I reinitialize the weight if a new LM is found? Probably not
+                S, innov = self.lm_filters[i][lm].update(z[:,j], Chi[:,i]) # S is innovation covariance
+                w[i] *= np.linalg.det(2 * np.pi * S)**(-0.5) * np.exp(-0.5 * (innov.T @ np.linalg.inv(S) @ innov))  #Is multiplying itself by the probability correct
         return Chi, w
 
     def lowVarianceSampling(self, Chi, w):  # May need to edit this to resample kalman filters also!!
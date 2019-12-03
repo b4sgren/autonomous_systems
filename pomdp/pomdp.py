113f7ff3bfc63265b156969ea9ce54511cd15cd2
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-02 21:09:52 -0700

    Have most of the algorithm coded but i'm not sure it does what I want yet

--- pomdp/pomdp.py
+++ pomdp/pomdp.py
@@ -14,18 +14,33 @@ class POMDPPlanner:
         debug = 1
     
     def createPolicy(self):
+        N = self.T.shape[1]
         for tau in range(self.steps):
             Yp = {}
             V = []
             for up, alphas in self.Y.items():
+                Vu = []
                 for u in range(self.T.shape[0]):
                     Vz = []
                     for z in range(self.Z.shape[1]):
-                        for j in range(len(alphas)):
+                        Vj = []
+                        for j in range(N):
                             v = np.array(alphas).T #Each column is an alpha vector
                             pz_x = self.Z[z,:]
                             pxp_ux = self.T[u,j,:]
                             vp = np.sum(v * pz_x * pxp_ux, axis=1) # Not sure this is exactly what I want to do
-                            Vz.append(vp)
-                    V.append(Vz)
+                            Vj.append(vp)
+                        Vz.append(Vj)
+                    Vu.append(Vz)
+                V.append(Vu)
+            V = np.array(V)
             for u in range(self.T.shape[0]):
+                for k1 in range(self.Z.shape[0]):
+                    for k2 in range(self.Z.shape[0]):
+                        VV = []
+                        for i in range(N):
+                            r = self.R[i,u]
+                            v = np.sum(V[0,u,:,k1, i]) # Not sure this is right
+                            vp = self.gamma * (r + v)
+                            VV.append(vp)
+                            debug = 1

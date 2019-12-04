65c1c1d944ac41b6d741b2eb2f2125f2a595f08d
Brendon Forsgren <bforsgren29@gmail.com>
2019-12-03 19:55:21 -0700

    Need to think of a better way to organize V for easier access in the second part. I'm pretty sure that the first part is right

--- pomdp/pomdp.py
+++ pomdp/pomdp.py
@@ -7,7 +7,7 @@ class POMDPPlanner:
         self.T = params.T
         self.Z = params.Z
         self.R = params.R
-        self.Y = {0:[np.zeros(2) for i in range(self.T.shape[1])]}
+        self.Y = {0:[[0.0, 0.0] for i in range(self.T.shape[1])]}
         self.gamma = params.gamma
     
     def prune(self):
@@ -18,6 +18,7 @@ class POMDPPlanner:
         for tau in range(self.steps):
             Yp = {}
             V = []
+            K = len(self.Y.keys())
             for up, alphas in self.Y.items():
                 Vu = []
                 for u in range(self.T.shape[0]):
@@ -28,20 +29,22 @@ class POMDPPlanner:
                             v = np.array(alphas).T #Each column is an alpha vector
                             pz_x = self.Z[z,:]
                             pxp_ux = self.T[u,j,:]
-                            vp = np.sum(v * pz_x * pxp_ux, axis=1) # Not sure this is exactly what I want to do
-                            Vj.append(vp)
+                            vp = np.sum(v * pz_x * pxp_ux, axis=1) # I think that this is right
+                            Vj.append(vp)   # Not sure if this setup is the best way to do things
                         Vz.append(Vj)
                     Vu.append(Vz)
                 V.append(Vu)
             V = np.array(V)
             for u in range(self.T.shape[0]):
-                for k1 in range(self.Z.shape[0]):
-                    for k2 in range(self.Z.shape[0]):
+                for k1 in range(K):
+                    for k2 in range(K):
                         VV = []
                         for i in range(N):
                             r = self.R[i,u]
-                            v = np.sum(V[0,u,:,k1, i]) # Not sure this is right
-                            vp = self.gamma * (r + v)
+                            v1 = np.sum(V[0,u,:,k1, i]) # The 0 in the front is wrong. Need a new way to organize V
+                            v2 = np.sum(V[0,u,:,k2,i])
+                            vp = self.gamma * (r + v1 + v2)
+
                             VV.append(vp)
                             debug = 1
                         if not u in Yp.keys():
@@ -50,5 +53,5 @@ class POMDPPlanner:
                             if not VV in Yp[u]:
                                 Yp[u].append(VV)
             #Do pruning here
-            Y = Yp
+            self.Y = Yp
             debug = 1
